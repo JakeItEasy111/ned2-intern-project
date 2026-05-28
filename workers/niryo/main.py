a2f3b7297df8549e82fe235b703e1bbd8642b539
JakeItEasy111 <jhoffmann982@gmail.com>
2026-05-28 12:23:22 -0400

    added fake workers and removed unecessary volume folders

---
+++ workers/niryo/main.py
@@ -0,0 +1,30 @@
+
+from arq import ArqRedis
+from arq.connections import RedisSettings
+import sys
+sys.path.insert(0, "/app/shared")  # make shared/ importable
+from state import set_status, set_value
+from settings import REDIS_URL
+import uuid 
+import asyncio
+
+async def fake_niryo_worker(ctx, job):
+    job_id = job.get("job_id", str(uuid.uuid4()))
+
+    print('f"[{job_id}] moving robot...')
+    await asyncio.sleep(5)
+    print('f"[{job_id}] action complete')
+
+    return {
+        "status": "success",
+        "job_id": job_id
+    }
+
+class WorkerSettings:
+    functions = [fake_niryo_worker]  # list every job this worker handles
+
+    # parsed from REDIS_URL: host, port, password, db
+    redis_settings = RedisSettings.from_dsn(REDIS_URL)
+
+    # how many jobs to run concurrently in this process
+    max_jobs = 1
